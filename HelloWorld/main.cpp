11a9cf4b7de191b20b07f389ebb9cb96d2bbbfc3
Max Rimmer Larsen <maxrimmer@live.dk>
2016-06-18 11:34:26 +0200

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Max Larsen on 18/06/16.
+//  Copyright © 2016 Max Larsen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
